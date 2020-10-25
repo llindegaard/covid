7b190ce895906d83b208c91238575c532f11cb05
Lasse Lindegaard <l_lindegaard@yahoo.com>
2020-10-25 15:31:39 +0100

    tilrettet merge konflikt

--- figurer.R
+++ figurer.R
@@ -6,11 +6,7 @@ library(ggthemes)
 coviddata <- as.data.frame(read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM"))
 coviddata$dato <- as.Date(coviddata$dateRep, format = "%d/%m/%Y")
 coviddata$doedmil <- pmin(pmax(coviddata$deaths,0),400) / coviddata$popData2019 *10e6
-<<<<<<< HEAD
 coviddata <-coviddata[which(coviddata$month >= 6), ]
-=======
-coviddata <-coviddata[which(coviddata$month >= 5), ]
->>>>>>> 09895ac1754978e725153b720fa875f9e61cff6b
 
 eu_data <-coviddata[which(coviddata$continentExp == 'Europe' ), ]
 eu_data <- eu_data[-which(eu_data$geoId %in% c("AM","BA","AD","AL","GE","GI","LU","ME","SM","VA","MC","MD","MK","RU","IM","GG","AZ","SI","XK","RS","BY","JE","MT","UA")), ]
@@ -18,13 +14,9 @@ eu_data <- eu_data[-which(eu_data$geoId %in% c("AM","BA","AD","AL","GE","GI","LU
 ggplot(eu_data, aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_per_100000, colour = geoId)) +
   geom_line()  + #scale_color_colorblind()+ *
   scale_x_date(date_labels="%b",date_breaks  ="1 month" ,limits = as.Date(c('2020-05-01',Sys.Date())))  +
-<<<<<<< HEAD
   facet_wrap(vars( geoId), ncol = 6) + 
   theme(legend.position = "none", axis.title.y = element_blank(), axis.title.x =  element_blank(),plot.title = element_text(hjust = 0.5)) +
   ggtitle("Europa - 14 dages kumuleret incidens")
-=======
-  facet_wrap(vars( geoId), ncol = 6) + theme(legend.position = "none")
->>>>>>> 09895ac1754978e725153b720fa875f9e61cff6b
 
 # ggplot(eu_data, aes(x = dato, y = doedmil, colour = geoId)) +
 #   geom_line()  + #scale_color_colorblind()+ *
@@ -39,7 +31,6 @@ ggplot(ferie, aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_
   geom_line() + scale_color_colorblind() + facet_wrap(vars( geoId), ncol = 2) +
   geom_hline(aes(yintercept = 30, lty="dotted")) +
   # geom_hline(aes(yintercept = 30, linetype="dashed"))  + 
-<<<<<<< HEAD
   scale_x_date(date_labels="%b",date_breaks  ="1 month" ,limits = as.Date(c('2020-08-01',Sys.Date()))) + theme(legend.position = "none") +
   theme(legend.position = "none", axis.title.y = element_blank(), axis.title.x =  element_blank(),plot.title = element_text(hjust = 0.5)) +
   ggtitle("Ferielande - 14 dages kumuleret incidens")
@@ -48,12 +39,10 @@ ggplot(ferie, aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_
 # fdfsdf
 ggplot(subset(ferie, geoId %in% c("IT")), aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_per_100000, colour = geoId)) +
   geom_line() + scale_color_colorblind()+ 
-=======
   scale_x_date(date_labels="%b",date_breaks  ="1 month" ,limits = as.Date(c('2020-08-01',Sys.Date()))) + theme(legend.position = "none")
 # fdfsdf
 ggplot(subset(ferie, geoId %in% c("IT")), aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_per_100000, colour = geoId)) +
   geom_line() + scale_color_colorblind()+
->>>>>>> 09895ac1754978e725153b720fa875f9e61cff6b
   scale_x_date(date_labels="%b",date_breaks  ="1 month" ,limits = as.Date(c('2020-07-01',Sys.Date()))) 
 
 ggplot(subset(coviddata, geoId %in% c("DK")), aes(x = dato, y = Cumulative_number_for_14_days_of_COVID.19_cases_per_100000, colour = geoId)) +
