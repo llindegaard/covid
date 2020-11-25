1fa8fe794a06e9128cb8a8a27807ac64310c9c07
Lasse <l_lindegaard@yahoo.com>
2020-11-25 10:02:59 +0100

    Tilføjet kommune-figurer

--- SSIdata.R
+++ SSIdata.R
@@ -1,5 +1,5 @@
 temp <- tempfile()
-SSIfil <- c("https://files.ssi.dk/covid19/overvagning/data/data-epidemiologiske-rapport-24112020-2cca")
+SSIfil <- c("https://files.ssi.dk/covid19/overvagning/data/data-epidemiologiske-rapport-25112020-88op")
 download.file(paste0(SSIfil,".csv"),temp, mode="wb") # aa <- read_file()
 # listfiles <- lapply(q,read.csv2,sep=";")
 
@@ -16,7 +16,30 @@ Test_pos_over_time$Date <- as.Date(Test_pos_over_time$Date, format = "%Y-%m-%d")
 Test_pos_over_time$NewPositive <- as.numeric(Test_pos_over_time$NewPositive)
 
 library(ggplot2)
+library(reshape2)
 ggplot(Test_pos_over_time, aes(x = Date, y = NewPositive)) +
   geom_line() 
 
+
+Municipality_cases_time_series$date_sample <- as.Date(Municipality_cases_time_series$date_sample)
+mm <- melt(Municipality_cases_time_series, id='date_sample')
+`%notin%` <- Negate(`%in%`)
+
+ggplot(mm[which(mm$variable %notin% c("Copenhagen","Aarhus","Aalborg","Odense")),], aes(x = date_sample, y = value, color = variable)) +
+  geom_line() +
+  facet_wrap(vars( variable), ncol = 10) +
+  theme(legend.position = "none") +
+  stat_smooth(method=loess, formula = y ~ x, na.rm = TRUE )
+
+ggplot(mm[which(mm$variable %in% c("Copenhagen","Aarhus","Aalborg","Odense")),], aes(x = date_sample, y = value, color = variable)) +
+  geom_line() +
+  facet_wrap(vars( variable), ncol = 2) +
+  theme(legend.position = "none") +
+  stat_smooth(method=loess, formula = y ~ x, na.rm = TRUE )
+
+ggplot(mm[which(mm$variable %in% c("Allerød","Ballerup","Hillerød","Lyngby.Taarbæk","Furesø","Egedal","Herlev","Thisted","Slagelse")),], aes(x = date_sample, y = value, color = variable)) +
+  geom_line() +
+  facet_wrap(vars( variable), ncol = 3) +
+  theme(legend.position = "none") +
+  stat_smooth(method=loess, formula = y ~ x, na.rm = TRUE )
 
