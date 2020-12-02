4c8400052df5410fe3f279f016cbd599331db620
Lasse Lindegaard <l_lindegaard@yahoo.com>
2020-12-02 21:50:39 +0100

    tilføjet library og rettet faktorer til char

--- EUregioner.R
+++ EUregioner.R
@@ -1,7 +1,8 @@
 library(jsonlite)
+library(ggplot2)
 `%notin%` <- Negate(`%in%`)
 # EUregioner <- read_json("https://opendata.ecdc.europa.eu/covid19/subnationalcaseweekly/json/", simplifyVector = TRUE)
-EUregioner <- as.data.frame(read.csv("https://opendata.ecdc.europa.eu/covid19/subnationalcaseweekly/csv/", na.strings = "", fileEncoding = "UTF-8-BOM"))
+EUregioner <- as.data.frame(read.csv("https://opendata.ecdc.europa.eu/covid19/subnationalcaseweekly/csv/", na.strings = "", fileEncoding = "UTF-8-BOM",stringsAsFactors =  FALSE))
 aar <- strsplit(EUregioner$year_week, "-W")
 uge <- data.frame(matrix(as.numeric(unlist(aar)), nrow=length(aar), byrow=T))
 
@@ -14,7 +15,7 @@ ggplot(bbb[which(bbb$country == "France" & bbb$nuts_code %notin% c("FRY1","FRY2"
   theme(legend.position = "none") +
    ggtitle("Frankrigs regioner")
 
-ggplot(bbb[which(bbb$country == "Spain" & bbb$nuts_code %in% c("ES53","ES51")),], aes(x = ugenr, y = rate_14_day_per_100k/2, color = region_name)) +
+ggplot(bbb[which(bbb$country == "Spain" & bbb$nuts_code %in% c("ES53","ES51","ES70")),], aes(x = ugenr, y = rate_14_day_per_100k/2, color = region_name)) +
   geom_line() +
   geom_hline(aes(yintercept = 30,  color = 'green')) + # scale_color_colorblind() +
   # stat_smooth(method=loess , na.rm = TRUE ) +
@@ -25,13 +26,15 @@ ggplot(bbb[which(bbb$country == "Denmark" & bbb$nuts_code %notin% c("GL")),], ae
   # facet_wrap(vars( region_name), ncol = 2) +
   # theme(legend.position = "none") +
   ggtitle("Danmarks regioner")
-<<<<<<< HEAD
-
 
 ggplot(bbb[which(bbb$country == "Italy" & bbb$nuts_code %notin% c("ITC1", "ITC2","ITC4", "ITF1", "ITF2", "ITF3", "ITF5", "ITF6","ITG1", "ITG2", "ITH1", "ITH2", "ITH4")),], aes(x = ugenr, y = rate_14_day_per_100k/2, color = region_name)) +
   geom_line() + # scale_color_colorblind() +
   facet_wrap(vars( region_name), ncol = 4) +
   theme(legend.position = "none") +
   ggtitle("Italiens regioner")
-=======
->>>>>>> e237d7bac1f6909a968a25c5a4d3acb673733cc3
+
+ggplot(bbb[which(bbb$country == "Germany" & bbb$nuts_code %notin% c("ITC1", "ITC2","ITC4", "ITF1", "ITF2", "ITF3", "ITF5", "ITF6","ITG1", "ITG2", "ITH1", "ITH2", "ITH4")),], aes(x = ugenr, y = rate_14_day_per_100k/2, color = region_name)) +
+  geom_line() + # scale_color_colorblind() +
+  facet_wrap(vars( region_name), ncol = 4) +
+  theme(legend.position = "none") +
+  ggtitle("Tysklands regioner")
