c22a61d6e70d269e290129bd6c55ef3f0982a561
llindegaard <l_lindegaard@yahoo.com>
2021-04-16 09:13:04 +0200

    Droppet 2020

--- EUregioner.R
+++ EUregioner.R
@@ -1,4 +1,4 @@
-library(jsonlite)
+# library(jsonlite)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
@@ -7,6 +7,8 @@ EUregioner <- as.data.frame(read.csv("https://opendata.ecdc.europa.eu/covid19/su
                                      , fileEncoding = "UTF-8-BOM",stringsAsFactors =  FALSE))
 
 EUregioner <- separate(EUregioner, year_week, c("aar", "ugenr"), "-W", convert = TRUE)
+EUregioner$rate_14_day_per_100k <- pmax(EUregioner$rate_14_day_per_100k,0)
+EUregioner <- EUregioner[which(EUregioner$aar > 2020),]
 
 ggplot(EUregioner[which(EUregioner$country == "France" & EUregioner$nuts_code %notin% c("FRC", "FRH", "FRB", "FRD", "FRI", "FRM", "FRF", 
                                        "FRY5","FRY1","FRY2","FRY3","FRY4")),]
@@ -16,17 +18,18 @@ ggplot(EUregioner[which(EUregioner$country == "France" & EUregioner$nuts_code %n
   theme(legend.position = "none") +
    ggtitle("Frankrigs regioner")
 
-ggplot(EUregioner[which(EUregioner$country == "Spain" & EUregioner$nuts_code %in% c("ES53","ES51","ES70")),]
+ggplot(EUregioner[which(EUregioner$country == "Spain" & EUregioner$nuts_code %in% c("ES53","ES51","ES70", "ES30")),]
        , aes(x = ugenr, y = rate_14_day_per_100k/2, color = aar)) +
   geom_line() +
   facet_wrap(vars(region_name), ncol = 2) +
+  theme(legend.position = "none") +
   ggtitle("Spaniens regioner")
 
 ggplot(EUregioner[which(EUregioner$country == "Denmark" & EUregioner$nuts_code %notin% c("GL")),]
        , aes(x = ugenr, y = rate_14_day_per_100k/2, color = aar)) +
   geom_line() +
   facet_wrap(vars( region_name), ncol = 2) +
-  theme(legend.position = "none") +
+  theme(legend.position = "bottom") +
   ggtitle("Danmarks regioner")
 
 ggplot(EUregioner[which(EUregioner$country == "Italy" & EUregioner$nuts_code %notin% c("ITC1", "ITC2","ITC4", "ITF1", "ITF2", "ITF3", "ITF5", "ITF6","ITG1", "ITG2", "ITH1", "ITH2", "ITH4")),]
@@ -49,4 +52,11 @@ ggplot(EUregioner[which(EUregioner$country == "Sweden" & EUregioner$nuts_code %n
   facet_wrap(vars( region_name), ncol = 4) +
   theme(legend.position = "none") +
   ggtitle("Sveriges regioner")
+
+ggplot(EUregioner[which(EUregioner$country == "Norway" & EUregioner$nuts_code %notin% c("ITC1", "ITC2","ITC4", "ITF1", "ITF2", "ITF3", "ITF5", "ITF6","ITG1", "ITG2", "ITH1", "ITH2", "ITH4")),]
+       , aes(x = ugenr, y = rate_14_day_per_100k/2, color = aar)) +
+  geom_line() + 
+  facet_wrap(vars( region_name), ncol = 4) +
+  theme(legend.position = "none") +
+  ggtitle("Norges regioner")
 
